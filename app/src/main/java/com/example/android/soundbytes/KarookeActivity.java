b2dbfc397e3fc6558d1f5c0b65a1396cc24f1f09
ahmed-razic <ahmed.razic@gmail.com>
2021-01-23 21:45:33 +0100

    added themes, styles, values

---
+++ app/src/main/java/com/example/android/soundbytes/KarookeActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.soundbytes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KarookeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karooke);
+    }
+}
