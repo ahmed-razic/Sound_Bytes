f9e8f7b7908e59b6ac692e3bd9625f0c8973a2f9
ahmed-razic <ahmed.razic@gmail.com>
2021-01-23 23:01:32 +0100

    added activities

---
+++ app/src/main/java/com/example/android/soundbytes/DetailInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.soundbytes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deatil_info);
+    }
+}
